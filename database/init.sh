41ef0ae53103cc8e18a5de088e1216f4a7c89085
Randall <randall.eagan.kim@gmail.com>
2025-01-08 23:33:59 -0500

    Get postgres login from .env

--- database/init.sh
+++ database/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-psql -U postgres -f ./reibun-db.sql
-psql -U postgres -d reibun -f ./textsearch_ja.sql
+psql -U $POSTGRES_USER -f ./reibun-db.sql
+psql -U $POSTGRES_USER -d reibun -f ./textsearch_ja.sql
