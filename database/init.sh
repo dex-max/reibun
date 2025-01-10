1113d393bf783d02d585242f23561980781bbc22
Randall <randall.eagan.kim@gmail.com>
2025-01-10 16:54:31 -0500

    Move initial sql scripts to /scripts

--- database/init.sh
+++ database/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-psql -U $POSTGRES_USER -f ./reibun-db.sql
-psql -U $POSTGRES_USER -d reibun -f ./textsearch_ja.sql
+psql -U $POSTGRES_USER -f /scripts/reibun-db.sql
+psql -U $POSTGRES_USER -d reibun -f /scripts/textsearch_ja.sql
