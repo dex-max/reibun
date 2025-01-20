526fc18ca874c92684c61615f0bbd87e35bcb6cd
Randall <randall.eagan.kim@gmail.com>
2025-01-19 23:20:27 -0500

    Add sentence search

--- api/app.py
+++ api/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from db import SentenceDB
 
 app = Flask(__name__)
+sentenceDB = SentenceDB()
 
-@app.route("/")
-def hello_world():
-    return "Hello World"
+@app.route('/sentences')
+def get_sentences():
+    search_term = request.args.get('search-term')
+
+    if search_term is None:
+        no_search_response = {
+            'error': { 'code': 400, 'message': 'No search term'}
+        }
+        return no_search_response, 400
+
+    response = {
+        'data': sentenceDB.search_sentences(search_term)
+    }
+
+    return response
--- api/db.py
+++ api/db.py
@@ -1,22 +1,39 @@
-import psycopg2
 import os
+from typing import cast, TypedDict
+
+from psycopg2 import connect, sql
+from psycopg2.extras import execute_batch, RealDictCursor
+
+class SentenceEntry(TypedDict):
+    content: str
 
 class SentenceDB:
     def __init__(self):
-
         user = os.environ.get('SQL_USERNAME')
         password = os.environ.get('SQL_PASSWORD')
         port = os.environ.get('SQL_PORT')
-        self.connection = psycopg2.connect(host='db', port=port, user=user, password=password, dbname="reibun")
+        self.connection = connect(host='db', port=port, user=user, password=password, dbname="reibun")
 
-    def search_sentences(self, search_term: str) -> [str]:
-        cursor = self.connection.cursor()
+    def search_sentences(self, search_term: str) -> list[SentenceEntry]:
+        cursor = self.connection.cursor(cursor_factory=RealDictCursor)
         
         cursor.execute("SELECT content FROM sentence WHERE to_tsvector('japanese', content) @@ to_tsquery('japanese', %s)", (search_term,))
-        return cursor.fetchall()
+        sentences = cursor.fetchall()
+        cursor.close()
 
-    def store_sentences(self, sentences: [str]) -> None:
+        return [cast(SentenceEntry, sentence) for sentence in sentences]
+
+    def store_sentences(self, sentences: list[SentenceEntry]) -> None:
         cursor = self.connection.cursor()
 
-        for sentence in sentences:
-            cursor.execute("INSERT INTO sentence (content) VALUES (%s)", (sentence,))
+        query = sql.SQL(
+            "INSERT INTO sentence {fields} VALUES {values}"
+        ).format(
+            fields=sql.SQL(',').join(map(sql.Identifier, SentenceEntry.__annotations__.keys())),
+            values=sql.SQL(',').join(sql.Placeholder() * len(SentenceEntry.__annotations__))
+        )
+
+        execute_batch(cursor, query, sentences)
+        
+        self.connection.commit()
+        cursor.close()
