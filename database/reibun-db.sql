ae99b33b0157166371ec22c907fc4c0de36af4ca
Randall <randall.eagan.kim@gmail.com>
2025-02-01 20:56:34 -0500

    Seed database with sentences from Tatoeba

--- database/reibun-db.sql
+++ database/reibun-db.sql
@@ -8,3 +8,9 @@ CREATE TABLE sentence (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL
 );
+
+COPY sentence (content)
+FROM '/sentences.tsv';
+
+CREATE INDEX content_vector_idx
+ON sentence USING GIN (to_tsvector('japanese', content));
