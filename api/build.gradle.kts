c3d9ade23575363eb80c6dc6be0984d919c724da
Randall <randall.eagan.kim@gmail.com>
2025-03-01 19:20:44 -0500

    Switch to EngineMain for ktor

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,7 +9,7 @@ group = "net.reibun"
 version = "0.0.1"
 
 application {
-    mainClass = "net.reibun.ApplicationKt"
+    mainClass = "io.ktor.server.netty.EngineMain"
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=true")
 }
 
@@ -24,10 +24,9 @@ dependencies {
     implementation(libs.postgresql)
     implementation(libs.h2)
     implementation(libs.ktor.server.cors)
-    implementation(libs.ktor.server.host.common)
-    implementation(libs.ktor.server.status.pages)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- api/src/main/kotlin/Application.kt
+++ api/src/main/kotlin/Application.kt
@@ -2,18 +2,10 @@ package net.reibun
 
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 
-fun main() {
-    embeddedServer(
-        Netty,
-        port = 8080,
-        host = "0.0.0.0",
-        module = Application::module,
-        watchPaths = listOf("classes")
-    ).start(wait = true)
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
